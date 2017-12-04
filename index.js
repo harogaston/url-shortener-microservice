b811dfbdac31ce7949a21c74122f9fd1028d6719
Gastón Haro <gaston.haro@fing.edu.uy>
2017-12-04 09:25:19 -0300

    asa

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-var http = require('http');
-var main = require('main');
 
-var app = express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .use('/', main);
+var app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-app.set('port', PORT);
-
-var server = http.createServer(app);
-
-server.listen(PORT);
-
-module.exports(app);
+app.get('/', (req, res) => res.render('pages/index'))
